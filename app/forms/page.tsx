e4f67ad95b6a1bd490e7f74389e1a2344c423c6e
Dhanavadh T. Saito <105502355+dhanavadh@users.noreply.github.com>
2025-11-29 17:16:04 +0700

    Update page.tsx

--- app/forms/page.tsx
+++ app/forms/page.tsx
@@ -3,7 +3,7 @@ import FormTemplateList from "@/app/components/Main/FormTemplateList";
 export default function FormsPage() {
     return (
         <main>
-            <FormTemplateList background="default" />
+            <FormTemplateList />
         </main>
     );
 }
